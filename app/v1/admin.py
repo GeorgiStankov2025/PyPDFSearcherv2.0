6aa84886c474faccf10b9b5484d5b45c0ec94949
GeorgiStankov2025 <georgistankov05@gmail.com>
2026-02-25 13:14:33 +0200

    Add-Admin-Endpoints-For-Role-Control-And-Delete

--- app/v1/admin.py
+++ app/v1/admin.py
@@ -1,7 +1,7 @@
 import uuid
 
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy import select
+from sqlalchemy import select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
@@ -48,3 +48,64 @@ async def get_user(id: str,session: AsyncSession = Depends(db.get_async_session)
     except Exception as e:
         raise HTTPException(status_code=500,detail=str(e))
 
+@router.patch("/change_user_role_to_admin/{id}", dependencies=[Depends(verify_token)],tags=["admin"])
+async def change_user_role_to_admin(id:str,session:AsyncSession=Depends(db.get_async_session),current_user:dict=Depends(get_current_user)):
+    try:
+        admin_search = await session.execute(select(User).where(User.username == current_user["username"]))
+        admin = admin_search.scalars().first()
+        if admin.user_role != UserRole.ADMIN:
+            raise HTTPException(status_code=403, detail="You are not authorized to view this page")
+        user_uuid = uuid.UUID(id)
+        result = await session.execute(select(User).where(User.id == user_uuid))
+        user = result.scalars().first()
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        if user.user_role==UserRole.ADMIN:
+            raise HTTPException(status_code=400,detail="User is already admin.")
+        user.user_role = UserRole.ADMIN
+        await session.commit()
+        await session.refresh(user)
+        return user
+    except HTTPException as e:
+        raise HTTPException(status_code=e.status_code,detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500,detail=str(e))
+
+@router.delete("/users/{id}",dependencies=[Depends(verify_token)],tags=["admin"])
+async def delete_user(id:str,session:AsyncSession=Depends(db.get_async_session),current_user:dict=Depends(get_current_user)):
+    try:
+        admin_search = await session.execute(select(User).where(User.username == current_user["username"]))
+        admin = admin_search.scalars().first()
+        if admin.user_role != UserRole.ADMIN:
+            raise HTTPException(status_code=403,detail="You are not authorized to view this page")
+        user_uuid=uuid.UUID(id)
+        result = await session.execute(select(User).where(User.id == user_uuid))
+        user=result.scalars().first()
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        await session.delete(user)
+        await session.commit()
+        return {"message":"User deleted successfully"}
+
+    except HTTPException as e:
+        raise HTTPException(status_code=e.status_code,detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500,detail=str(e))
+
+@router.patch("/demote_admin_to_user/{id}",dependencies=[Depends(verify_token)],tags=["admin"])
+async def demote_admin_to_user(id:str,session:AsyncSession=Depends(db.get_async_session),current_user:dict=Depends(get_current_user)):
+    admin_search = await session.execute(select(User).where(User.username == current_user["username"]))
+    admin = admin_search.scalars().first()
+    if admin.user_role != UserRole.ADMIN:
+        raise HTTPException(status_code=403, detail="You are not authorized to view this page")
+    user_uuid = uuid.UUID(id)
+    result = await session.execute(select(User).where(User.id == user_uuid))
+    user = result.scalars().first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    if user.user_role == UserRole.USER:
+        raise HTTPException(status_code=400, detail="User is already with a user role.")
+    user.user_role = UserRole.USER
+    await session.commit()
+    await session.refresh(user)
+    return user
