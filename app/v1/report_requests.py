71180e77ce6e5b9bccf00561b34a9ad92467ab31
GeorgiStankov2025 <georgistankov05@gmail.com>
2026-03-04 14:15:35 +0200

    Add-Download-Files-At-Report-Requests

--- app/v1/report_requests.py
+++ app/v1/report_requests.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.agent import invoke_reports_agent
 from app.db import get_async_session, ReportRequest, User
 from app.schemas import CreateReport
+from app.v1.files import download_file
 from app.v1.users import verify_token, get_current_user
 from docx import Document
 router=APIRouter(dependencies=[Depends(verify_token)])
@@ -36,10 +37,11 @@ async def create_report(request:CreateReport,session:AsyncSession=Depends(get_as
         document.add_heading(f"Author:{current_user['username']}",level=2)
         document.add_paragraph(document_content)
         document.save(rf"D:\ПУ\II курс\Python\PyPDFSearcher\generated_reports\{request.message}.docx")
+
     session.add(report_request)
     await session.commit()
     await session.refresh(report_request)
-    return report_request
+    return await download_file(request.message+".docx")
 
 @router.get("/report_requests",tags=["report_requests"])
 async def get_all_report_requests(session:AsyncSession=Depends(get_async_session)):
